9010e004688e534f3bd8cf69e104b5910c73c63c
kdaily <kdaily@users.noreply.github.com>
2015-12-03 16:20:55 -0800

    updated server

--- server.R
+++ server.R
@@ -1,16 +1,36 @@
-Author : Duncan P.
-# Server file of getCookie Shiny application. 
-library(synapseClient)
 
-shinyServer(function(input, output,session) { 
-  observeEvent(input$readCookie, {
-    session$sendCustomMessage(type="readCookie",message=list(name=input$cookieName))}) 
-  observeEvent(input$cookie, {
-    synapseAuthServiceEndpoint('https://auth-staging.prod.sagebase.org/auth/v1')
-    synapseRepoServiceEndpoint('https://repo-staging.prod.sagebase.org/repo/v1')
-    synapseFileServiceEndpoint('https://file-staging.prod.sagebase.org/file/v1')
-    synapseLogin(sessionToken=input$cookie)
-    output$profileName <- renderText({synGetUserProfile()@displayName})
-    })
-    
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+# This server has been modified to be used specifically on Sage Bionetworks Synapse pages
+# to log into Synapse as the currently logged in user from the web portal using the session token.
+#
+# https://www.synapse.org
+
+library(shiny)
+
+shinyServer(function(input, output, session) {
+
+  session$sendCustomMessage(type="readCookie",
+                            message=list(name='org.sagebionetworks.security.user.login.token'))
+  
+  foo <- reactive(synapseLogin(sessionToken=input$cookie))
+
+  output$title <- renderUI({
+    titlePanel(sprintf("Welcome, %s", synGetUserProfile()@userName))
+  })
+  
+  output$distPlot <- renderPlot({
+
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+
+  })
+
 })
