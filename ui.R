3f79489c536b4361cced4f1a5a64d11038705f69
kdaily <kdaily@users.noreply.github.com>
2015-12-03 16:21:03 -0800

    updated ui

--- ui.R
+++ ui.R
@@ -1,24 +1,40 @@
-Author : Duncan P.
-# UI file of getCookie Shiny application.
-shinyUI(bootstrapPage(
-  tags$head(tags$script(
-                  HTML('
-                        Shiny.addCustomMessageHandler("readCookie", function(message) {
-                          var cookie = readCookie(message.name);
-                          Shiny.onInputChange("cookie",cookie);
-                        })
-                        function readCookie(name) {
-                          var nameEQ = name + "=";
-                          var ca = document.cookie.split(";");
-                          for(var i=0;i < ca.length;i++) {
-                            var c = ca[i];
-                            while (c.charAt(0)==" ") c = c.substring(1,c.length);
-                            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-                            }   
-                            return null;
-                        }'))),
-            mainPanel(
-              actionButton('readCookie','Read Cookie'),
-              textInput('cookieName','Name of Cookie')),
-              uiOutput('profileName')
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+# This interface has been modified to be used specifically on Sage Bionetworks Synapse pages
+# to log into Synapse as the currently logged in user from the web portal using the session token.
+#
+# https://www.synapse.org
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+  tags$head(
+    singleton(
+      includeScript("www/readCookie.js")
+    )
+  ),
+  
+  # Application title
+  uiOutput("title"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
 ))
