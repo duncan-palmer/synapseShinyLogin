34be0117e0b61b77a4439029ebdec511fd20d5e7
duncan-palmer <duncan.palmer@sagebase.org>
2015-12-11 10:07:54 -0800

    Revised authentication code and placed JS code in its own separate file

--- server.R
+++ server.R
@@ -1,16 +1,12 @@
 Author : Duncan P.
 # Server file of getCookie Shiny application. 
 library(synapseClient)
-
 shinyServer(function(input, output,session) { 
   observeEvent(input$readCookie, {
     session$sendCustomMessage(type="readCookie",message=list(name=input$cookieName))}) 
-  observeEvent(input$cookie, {
-    synapseAuthServiceEndpoint('https://auth-staging.prod.sagebase.org/auth/v1')
-    synapseRepoServiceEndpoint('https://repo-staging.prod.sagebase.org/repo/v1')
-    synapseFileServiceEndpoint('https://file-staging.prod.sagebase.org/file/v1')
+
+observeEvent(input$cookie, { 
     synapseLogin(sessionToken=input$cookie)
-    output$profileName <- renderText({synGetUserProfile()@displayName})
-    })
-    
-})
+    uName <- synGetUserProfile()@displayName
+    title <- paste('Hello',uName,'- you\'ve logged in!',sep=' ')
+    output$nameGraph <- renderPlot({barplot(table(strsplit(uName,'')),xlab='Characters',ylab='Frequency',main=title)})}))
--- ui.R
+++ ui.R
@@ -2,23 +2,6 @@ Author : Duncan P.
 # UI file of getCookie Shiny application.
 shinyUI(bootstrapPage(
   tags$head(tags$script(
-                  HTML('
-                        Shiny.addCustomMessageHandler("readCookie", function(message) {
-                          var cookie = readCookie(message.name);
-                          Shiny.onInputChange("cookie",cookie);
-                        })
-                        function readCookie(name) {
-                          var nameEQ = name + "=";
-                          var ca = document.cookie.split(";");
-                          for(var i=0;i < ca.length;i++) {
-                            var c = ca[i];
-                            while (c.charAt(0)==" ") c = c.substring(1,c.length);
-                            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-                            }   
-                            return null;
-                        }'))),
-            mainPanel(
-              actionButton('readCookie','Read Cookie'),
-              textInput('cookieName','Name of Cookie')),
-              uiOutput('profileName')
+            mainPanel(plotOutput('nameGraph'))
+))
 ))
